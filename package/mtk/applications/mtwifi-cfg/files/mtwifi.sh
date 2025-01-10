0024a71bc2083d3e79577e28046e4bd71d7593f8
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2025-01-10 12:47:31 +0800

    Update mtwifi.sh

--- package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+++ package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
@@ -20,14 +20,14 @@ detect_mtwifi() {
 					hwmode="11g"
 					htmode="HE40"
 					htbsscoex="1"
-					ssid="ImmortalWrt-2.4G"
+					ssid="HiWiFi"
 					dbdc_main="1"
 				else
 					band="5g"
 					hwmode="11a"
 					htmode="HE160"
 					htbsscoex="0"
-					ssid="ImmortalWrt-5G"
+					ssid="HiWiFi_5G"
 					dbdc_main="0"
 				fi
 				uci -q batch <<-EOF
@@ -40,7 +40,7 @@ detect_mtwifi() {
 					set wireless.${dev}.channel=auto
 					set wireless.${dev}.txpower=100
 					set wireless.${dev}.htmode=${htmode}
-					set wireless.${dev}.country=CN
+					set wireless.${dev}.country=AU
 					set wireless.${dev}.mu_beamformer=1
 					set wireless.${dev}.noscan=${htbsscoex}
 					set wireless.${dev}.serialize=1
@@ -50,7 +50,7 @@ detect_mtwifi() {
 					set wireless.default_${dev}.network=lan
 					set wireless.default_${dev}.mode=ap
 					set wireless.default_${dev}.ssid=${ssid}
-					set wireless.default_${dev}.encryption=none
+					set wireless.default_${dev}.encryption=he1235678
 EOF
 				uci -q commit wireless
 			}
